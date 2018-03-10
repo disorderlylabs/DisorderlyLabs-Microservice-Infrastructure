73d81a965be3c1b711d934c28700486cbb8c3a20
UmangSardesai <umang2610@gmail.com>
2018-03-10 12:52:58 -0800

    Zipkin works. Using default bridge IP instead of internal subnet IP

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-docker run -d -p 7001:8080 --net=mynet --ip=10.0.0.21 -e zipkin_ip=10.0.0.25:9411  com.disorderlylabs/inventory
-docker run -d -p 7002:8080 --net=mynet --ip=10.0.0.22 -e zipkin_ip=10.0.0.25:9411 -e inventory_ip=10.0.0.21:8080 -e invoice_ip=10.0.0.23:8080 -e pg_ip=10.0.0.24:8080 com.disorderlylabs/cart
-docker run -d -p 7003:8080 --net=mynet --ip=10.0.0.23 -e zipkin_ip=10.0.0.25:9411 -e cart_ip=10.0.0.22:8080 -e inventory_ip=10.0.0.21:8080 -e pg_ip=10.0.0.24:8080 com.disorderlylabs/invoice
-docker run -d -p 7004:8080 --net=mynet --ip=10.0.0.24 -e zipkin_ip=10.0.0.25:9411 com.disorderlylabs/paymentgateway
-docker run -d -p 7000:8080 --net=mynet --ip=10.0.0.20 -e zipkin_ip=10.0.0.25:9411 -e inventory_ip=10.0.0.21:8080 -e cart_ip=10.0.0.22:8080 -e invoice_ip=10.0.0.23:8080 com.disorderlylabs/app
-docker run -d -p 9411:9411 --net=mynet --ip=10.0.0.25 openzipkin/zipkin
+docker run -d -p 7001:8080 --net=mynet --ip=10.0.0.21 -e zipkin_ip=172.17.0.1:9411  com.disorderlylabs/inventory
+docker run -d -p 7002:8080 --net=mynet --ip=10.0.0.22 -e zipkin_ip=172.17.0.1:9411 -e inventory_ip=10.0.0.21:8080 -e invoice_ip=10.0.0.23:8080 -e pg_ip=10.0.0.24:8080 com.disorderlylabs/cart
+docker run -d -p 7003:8080 --net=mynet --ip=10.0.0.23 -e zipkin_ip=172.17.0.1:9411 -e cart_ip=10.0.0.22:8080 -e inventory_ip=10.0.0.21:8080 -e pg_ip=10.0.0.24:8080 com.disorderlylabs/invoice
+docker run -d -p 7004:8080 --net=mynet --ip=10.0.0.24 -e zipkin_ip=172.17.0.1:9411 com.disorderlylabs/paymentgateway
+docker run -d -p 7000:8080 --net=mynet --ip=10.0.0.20 -e zipkin_ip=172.17.0.1:9411 -e inventory_ip=10.0.0.21:8080 -e cart_ip=10.0.0.22:8080 -e invoice_ip=10.0.0.23:8080 com.disorderlylabs/app
+docker run -d -p 9411:9411 --net=mynet openzipkin/zipkin
